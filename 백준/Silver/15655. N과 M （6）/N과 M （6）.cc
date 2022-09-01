2ca87b60e076ac1891011d9f47f8138d97d662f4
mouseku <64977390+mouseku@users.noreply.github.com>
2022-09-01 15:45:24 +0900

    [Silver III] Title: N과 M (6), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.cc
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m;
+int is_used[10];
+int val[10];
+int num[10];
+
+
+void func(int k){
+    if(k == m){
+        for(int i=0; i<m; i++){
+            cout << num[i] << ' ';
+        }
+        cout << '\n';
+        return;
+    }
+    for(int i=0; i<n; i++){
+        if(is_used[i]) continue;
+        if(num[k-1]>val[i]) continue;
+        is_used[i] = 1;
+        num[k] = val[i];
+        func(k+1);
+        is_used[i] = 0;
+    }
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n >> m;
+    for(int i=0; i<n; i++){
+        cin >> val[i];
+    }
+    sort(val, val+n);
+    func(0);
+    return 0;
+}
