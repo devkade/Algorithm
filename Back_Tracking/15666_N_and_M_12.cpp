6ca92ca7d0abab17fd36b1062b1d838181085de0
Donggle <mouseku@naver.com>
2022-09-08 16:35:00 +0900

    09/08 [BOJ] 15666: N과 M (12) https://www.acmicpc.net/problem/15666

---
+++ Back_Tracking/15666_N_and_M_12.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m;
+int val[10];
+int num[10];
+
+void func(int k){
+    if(k == m){
+        for(int i=0; i<m; i++){
+            cout << num[i] << " ";
+        }
+        cout << '\n';
+        return ;
+    }
+    int tmp = 0;
+    for(int i=0; i<n; i++){
+        if(tmp == val[i]) continue;
+        if(k!=0 && num[k-1] > val[i]) continue;
+        num[k] = val[i];
+        tmp = num[k];
+        func(k+1);
+    }
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n >> m;
+    for(int i=0; i<n; i++){
+        cin >> val[i];
+    }
+    sort(val, val+n);
+    func(0);
+    return 0;
+}
