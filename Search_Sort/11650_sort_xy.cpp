78e3a1965eba29b728f32c767ff5338f9fc10da8
Kade <mouseku43@gmail.com>
2023-08-29 21:54:42 +0900

    [BOJ] 11650/11651 좌표 정렬하기

---
+++ Search_Sort/11650_sort_xy.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+vector <pair<int, int>> vec;
+
+bool compare(const pair<int, int> &a, const pair<int, int> &b){
+    if (a.first == b.first){
+        return a.second < b.second;
+    }
+    else{
+        return a.first < b.first;
+    }
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n, x, y;
+
+    cin >> n;
+
+    for(int i=0; i<n; i++){
+        cin >> x >> y;
+        vec.push_back({x, y});
+    }
+    sort(vec.begin(), vec.end(), compare);
+
+    for(int i=0; i<n; i++){
+        cout << vec[i].first << " " << vec[i].second << '\n';
+    }
+
+    return 0;
+}
