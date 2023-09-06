a6fc99f66c3393c79f9f689f92f2ff19645a91a4
Kade <mouseku43@gmail.com>
2023-09-07 02:08:23 +0900

    [BOJ] 7562: 나이트의 이동 2회차

--- Graph/BFS_DFS/7562_MovingKnight.cpp
+++ Graph/BFS_DFS/7562_MovingKnight.cpp
@@ -59,3 +59,59 @@ int main(){
         }
     }
 }
+
+
+/* ----------------------------------------------------------------2??
+#include <bits/stdc++.h>
+using namespace std;
+#define X first
+#define Y second
+
+int dx[8] = {-1, -1, 1, 1, -2, -2, 2, 2};
+int dy[8] = {-2, 2, -2, 2, -1, 1, -1, 1};
+int T, I;
+queue<pair<int, int>> q;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    pair<int, int> start, end;
+
+    cin >> T;
+
+    
+    while(T--){
+        int board[303][303] = {0,};
+        int dist[303][303] = {0,};
+        cin >> I;
+        cin >> start.X >> start.Y;
+        q.push(start);
+        cin >> end.X >> end.Y;
+        
+
+        if (start.X == end.X && start.Y == end.Y) {
+            cout << 0 << '\n';
+            continue;
+        }
+        while(!q.empty()){
+            auto cur = q.front(); q.pop();
+            for(int dir = 0; dir < 8; dir++){
+                int nx = cur.X + dx[dir];
+                int ny = cur.Y + dy[dir];
+                if(nx < 0 || nx >= I || ny < 0 || ny >= I) continue;
+                if(dist[nx][ny] > 0) continue;
+                dist[nx][ny] = dist[cur.X][cur.Y]+1;
+                if(nx == end.X && ny == end.Y){
+                    cout << dist[nx][ny] << '\n';
+                    break;
+                }
+                q.push({nx, ny});
+            }
+        }
+        
+    }
+
+    return 0;
+}
+*/
